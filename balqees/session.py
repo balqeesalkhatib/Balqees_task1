bd9bcde724186373094d3c07b0f1b2f7333005bc
balqeesalkhatib <balqeesalkhatib95@gmail.com>
2022-10-01 13:25:48 +0300

    first task

---
+++ balqees/session.py
@@ -0,0 +1,9 @@
+print("balqees")
+print("balqees\n","26")
+print("Alaa almohsen \n 20")
+name= "balqees"
+age="26"
+major="E E "
+print("my name is {}, age{}, major{}".format(name,age,major))
+print(2^2)
+print("bassem" and "ahmad")
