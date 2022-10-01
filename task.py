33b7d0ffe1f19df295d64dc7e784ed2aa7323129
balqeesalkhatib <balqeesalkhatib95@gmail.com>
2022-10-01 15:01:33 +0300

    task

--- task.py
+++ task.py
@@ -1,1 +1,16 @@
-print(1+0)
+#------------- length-----------
+len=int(input("input your length"))
+#my length is 173
+if len  >= 170 :
+     print("tall")
+else:
+     print("short")
+#------------- leap year-----------
+# the year number must be divisible by four, except for end of the end of the century should divisable by 400
+year= 2022
+if (year % 400 == 0) and (year % 100 == 0):
+        print("2022 is leap year")
+elif (year % 4 == 0) and (year % 100 != 0) :
+        print("2022 is leap year")
+else:
+    print("2022 is  notleap year")
